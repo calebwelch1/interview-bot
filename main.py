64cc27aeedb9a8336d6d6b246dd9421f358b7b35
Caleb <calebjohn@posteo.net>
2023-12-01 18:37:50 -0500

    notes on try catch

--- main.py
+++ main.py
@@ -165,3 +165,29 @@ def text_to_speech(text):
             print('something went wrong!')
     except Exception as e:
         print(e)
+
+
+
+
+# import openai
+# from openai import OpenAI
+# client = OpenAI()
+
+# try:
+#   #Make your OpenAI API request here
+#   response = client.completions.create(
+#     prompt="Hello world",
+#     model="gpt-3.5-turbo-instruct"
+#   )
+# except openai.APIError as e:
+#   #Handle API error here, e.g. retry or log
+#   print(f"OpenAI API returned an API Error: {e}")
+#   pass
+# except openai.APIConnectionError as e:
+#   #Handle connection error here
+#   print(f"Failed to connect to OpenAI API: {e}")
+#   pass
+# except openai.RateLimitError as e:
+#   #Handle rate limit error (we recommend using exponential backoff)
+#   print(f"OpenAI API request exceeded rate limit: {e}")
+#   pass
